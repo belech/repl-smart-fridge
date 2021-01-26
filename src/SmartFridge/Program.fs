d2d9e8cfb5399ccfb737549b5ec4d55e7250ebe8
Peter <seltenhammer.peter@gmail.com>
2021-01-26 19:10:45 +0100

    add ascii graphic to improve brand recognition

--- src/SmartFridge/Program.fs
+++ src/SmartFridge/Program.fs
@@ -1,7 +1,13 @@
-﻿[<EntryPoint>]
+[<EntryPoint>]
 
 let main argv =
     printfn "Welcome to the Smart Fridge!"
+    printfn "   _____ __  __          _____ _______   ______ _____  _____ _____   _____ ______ "
+    printfn "  / ____|  \/  |   /\   |  __ \__   __| |  ____|  __ \|_   _|  __ \ / ____|  ____|"
+    printfn " | (___ | \  / |  /  \  | |__) | | |    | |__  | |__) | | | | |  | | |  __| |__   "
+    printfn "  \___ \| |\/| | / /\ \ |  _  /  | |    |  __| |  _  /  | | | |  | | | |_ |  __|  "
+    printfn "  ____) | |  | |/ ____ \| | \ \  | |    | |    | | \ \ _| |_| |__| | |__| | |____ "
+    printfn " |_____/|_|  |_/_/    \_\_|  \_\ |_|    |_|    |_|  \_\_____|_____/ \_____|______|"
     printfn "Please enter your commands to interact with the system."
     printfn "Press CTRL+C to stop the program."
     printf "> "
